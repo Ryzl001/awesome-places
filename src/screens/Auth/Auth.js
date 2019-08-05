da94f53d642ec4ae2b27e05c706bbf631a2e48b6
izicase <admin@izicase.pl>
2019-08-05 21:02:42 +0200

    adding background image

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import { View, Text, Button, StyleSheet, TextInput } from "react-native";
+import { View, Text, Button, StyleSheet, ImageBackground } from "react-native";
 
 import startMainTabs from "../MainTabs/startMainTabs";
 import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
 import HeadingText from "../../components/UI/HeadingText/HeadingText";
 import MainText from "../../components/UI/MainText/MainText";
+
+import backgroundImage from "../../assets/background.jpg";
 
 class AuthScreen extends Component {
   loginHandler = () => {
@@ -13,20 +15,22 @@ class AuthScreen extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <MainText>
-          <HeadingText>Please Log In</HeadingText>
-        </MainText>
-        <Button title="Switch to Login" />
-        <View style={styles.inputContainer}>
-          <DefaultInput placeholder="Your E-mail Address" style={styles.input} />
-          <DefaultInput placeholder="Password" style={styles.input} />
-          {/* możemy wstawiać kolejne style jako element tabelii */}
-          {/* <DefaultInput placeholder="Password" style={[styles.input, { borderColor: "red" }]} /> */}
-          <DefaultInput placeholder="Confirm Password" style={styles.input} />
+      <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Please Log In</HeadingText>
+          </MainText>
+          <Button title="Switch to Login" />
+          <View style={styles.inputContainer}>
+            <DefaultInput placeholder="Your E-mail Address" style={styles.input} />
+            <DefaultInput placeholder="Password" style={styles.input} />
+            {/* możemy wstawiać kolejne style jako element tabelii */}
+            {/* <DefaultInput placeholder="Password" style={[styles.input, { borderColor: "red" }]} /> */}
+            <DefaultInput placeholder="Confirm Password" style={styles.input} />
+          </View>
+          <Button title="Submit" onPress={this.loginHandler} />
         </View>
-        <Button title="Submit" onPress={this.loginHandler} />
-      </View>
+      </ImageBackground>
     );
   }
 }
@@ -46,6 +50,10 @@ const styles = StyleSheet.create({
   input: {
     backgroundColor: "#eee",
     borderColor: "#bbb"
+  },
+  backgroundImage: {
+    width: "100%",
+    flex: 1
   }
 });
 
