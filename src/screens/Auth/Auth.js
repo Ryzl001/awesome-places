c2a273820a0c60e7d6407392103fb549a4f9c0af
izicase <admin@izicase.pl>
2019-08-05 19:42:07 +0200

    flebox in action and first styling

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, Button, StyleSheet, TextInput } from "react-native";
 
-import startMainTabs from '../MainTabs/startMainTabs';
+import startMainTabs from "../MainTabs/startMainTabs";
 
 class AuthScreen extends Component {
-    loginHandler = () => {
-        startMainTabs();
-    }
+  loginHandler = () => {
+    startMainTabs();
+  };
 
-    render () {
-        return (
-            <View>
-                <Text>Auth Screen</Text>
-                <Button title="Login" onPress={this.loginHandler}/>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Please Log In</Text>
+        <Button title="Switch to Login" />
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Your E-mail Address" style={styles.input} />
+          <TextInput placeholder="Password" style={styles.input} />
+          <TextInput placeholder="Confirm Password" style={styles.input} />
+        </View>
+        <Button title="Submit" onPress={this.loginHandler} />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    // flex 1 rozciąga element na cały dostępny obszar
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+    // borderColor: "red",
+    // borderWidth: 1
+  },
+  inputContainer: {
+    width: "80%"
+  },
+  input: {
+    width: "100%"
+  }
+});
 
 export default AuthScreen;
