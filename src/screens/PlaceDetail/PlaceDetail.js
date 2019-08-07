e67902afa470a86d37e875560c7bc1d1f09bba23
izicase <admin@izicase.pl>
2019-08-07 15:37:19 +0200

    styling PlaceDetail responsive

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
-import { View, Image, Text, Button, StyleSheet, TouchableOpacity, Platform } from "react-native";
+import {
+  View,
+  Image,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  Dimensions
+} from "react-native";
 import { connect } from "react-redux";
 
 import Icon from "react-native-vector-icons/Ionicons";
 import { deletePlace } from "../../store/actions/index";
 
 class PlaceDetail extends Component {
+  state = {
+    viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape"
+  };
+  constructor(props) {
+    super(props);
+    Dimensions.addEventListener("change", this.updateStyles);
+  }
+
+  componentWillUnmount() {
+    Dimensions.removeEventListener("change", this.updateStyles);
+  }
+
+  updateStyles = dims => {
+    this.setState({
+      viewMode: dims.window.height > 500 ? "portrait" : "landscape"
+    });
+  };
+
   placeDeletedHandler = () => {
     this.props.onDeletePlace(this.props.selectedPlace.key);
     this.props.navigator.pop();
@@ -13,12 +40,26 @@ class PlaceDetail extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <View
+        style={
+          this.state.viewMode === "portrait"
+            ? styles.portraitContainer
+            : styles.landscapeContainer
+        }
+      >
         <View>
-          <Image source={this.props.selectedPlace.image} style={styles.placeImage} />
-          <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+          <Image
+            source={this.props.selectedPlace.image}
+            style={
+              this.state.viewMode === "portrait"
+                ? styles.portraitPlaceImage
+                : styles.landscapePlaceImage
+            }
+          />
         </View>
-        <View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+
           <TouchableOpacity onPress={this.placeDeletedHandler}>
             <View style={styles.deleteButton}>
               <Icon
@@ -35,12 +76,32 @@ class PlaceDetail extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  portraitContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-around",
     margin: 5
   },
-  placeImage: {
+  landscapeContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    margin: 5
+  },
+  infoContainer: {
+    flex: 1,
+    height: "100%",
+    flexDirection: "column",
+    justifyContent: "space-around"
+  },
+  portraitPlaceImage: {
     width: "100%",
     height: 200
+  },
+  landscapePlaceImage: {
+    width: 300,
+    height: "100%"
   },
   placeName: {
     fontWeight: "bold",
