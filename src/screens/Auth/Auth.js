774167e9a05916eff92743bb70010c7f7ca4f27d
izicase <admin@izicase.pl>
2019-08-07 12:15:23 +0200

    Adjusting Styles Dynamically

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -10,13 +10,57 @@ import ButtonWithBackground from "../../components/UI/ButtonWithBackground/Butto
 import backgroundImage from "../../assets/background.jpg";
 
 class AuthScreen extends Component {
+  state = {
+    respStyles: {
+      pwContainerDirection: "column",
+      pwContainerJustifyContent: "flex-start",
+      pwWrapperWidth: "100%"
+    }
+  };
+
+  constructor(props) {
+    super(props);
+    Dimensions.addEventListener("change", dims => {
+      // if (dims.window.height < 500) {
+      //   this.setState({
+      //     respStyles: {
+      //       pwContainerDirection: "row",
+      //       pwContainerJustifyContent: "space-between",
+      //       pwWrapperWidth: "49.5%"
+      //     }
+      //   });
+      // } else {
+      //   this.setState({
+      //     respStyles: {
+      //       pwContainerDirection: "column",
+      //       pwContainerJustifyContent: "flex-start",
+      //       pwWrapperWidth: "100%"
+      //       // pwContainerDirection: Dimensions.get("window").height > 500 ? "column" : "row",
+      //       // pwContainerJustifyContent:
+      //       //   Dimensions.get("window").height > 500 ? "flex-start" : "space-between",
+      //       // pwWrapperWidth: Dimensions.get("window").height > 500 ? "100%" : "49.5%"
+      //     }
+      //   });
+      // }
+
+      this.setState({
+        respStyles: {
+          pwContainerDirection: Dimensions.get("window").height > 500 ? "column" : "row",
+          pwContainerJustifyContent:
+            Dimensions.get("window").height > 500 ? "flex-start" : "space-between",
+          pwWrapperWidth: Dimensions.get("window").height > 500 ? "100%" : "49.5%"
+        }
+      });
+    });
+  }
+
   loginHandler = () => {
     startMainTabs();
   };
 
   render() {
     let headintText = null;
-    console.log(Dimensions.get("window"));
+    // console.log(Dimensions.get("window"));
     if (Dimensions.get("window").height > 500) {
       headintText = (
         <MainText>
@@ -37,13 +81,18 @@ class AuthScreen extends Component {
           </ButtonWithBackground>
           <View style={styles.inputContainer}>
             <DefaultInput placeholder="Your E-mail Address" style={styles.input} />
-            <View style={styles.passwordContainer}>
-              <View style={styles.passwordWrapper}>
+            <View
+              style={{
+                flexDirection: this.state.respStyles.pwContainerDirection,
+                justifyContent: this.state.respStyles.pwContainerJustifyContent
+              }}
+            >
+              <View style={{ width: this.state.respStyles.pwWrapperWidth }}>
                 <DefaultInput placeholder="Password" style={styles.input} />
               </View>
               {/* możemy wstawiać kolejne style jako element tabelii */}
               {/* <DefaultInput placeholder="Password" style={[styles.input, { borderColor: "red" }]} /> */}
-              <View style={styles.passwordWrapper}>
+              <View style={{ width: this.state.respStyles.pwWrapperWidth }}>
                 <DefaultInput placeholder="Confirm Password" style={styles.input} />
               </View>
             </View>
